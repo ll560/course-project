927de3fc20a1ccbe9df9007d535605f386ee57e8
ll560 <llavia6@gmail.com>
2021-05-13 20:25:46 -0400

    added get routes

--- app.js
+++ app.js
@@ -7,8 +7,24 @@ const PORT = 3000;
 app.use(morgan('combined'));
 
 //routes
-app.get("/views/pages/index.html", (request, response) => {
-    response.send("Hello");
+
+app.get("/", (request, response) =>{
+    response.send("This route points to the Home page");
+})
+app.get("/about", (request, response) =>{
+    response.send("This route points to the About page");
+})
+
+app.get("/login", (request, response) =>{
+    response.send("This route points to the Login page");
+})
+
+app.get("/admin-console", (request, response) =>{
+    response.send("This route points to the Admin Console page");
+})
+
+app.get("/admin-console/create-book", (request, response) =>{
+    response.send("This route points to the Create page");
 })
 
 app.listen(PORT, () => {
