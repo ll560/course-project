108ec87d1721d3c03941350253121cfb64a75178
ll560 <llavia6@gmail.com>
2021-05-21 15:29:31 -0400

    update the index-routes

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const adminRouter = require('./admin-routes.js');
+const bookRouter = require('./book-routes');
+const siteRouter = require('./site-routes');
+
+router.use('/admin-console', adminRouter);
+router.use('/books', bookRouter);
+router.use('/', siteRouter);
+
+module.exports = router;
